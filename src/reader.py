84e52221b54951c2038cf1d865d08ba6d2ecc21b
Moritz Kastler <moritz.kastler@outlook.com>
2024-10-01 12:08:07 +0200

    initial Setup

---
+++ src/reader.py
@@ -0,0 +1,5 @@
+"""
+    This file contains everything necessary for loading the mnist dataset, mainly the dataloader class.
+    It originates from: https://www.kaggle.com/code/hojjatk/read-mnist-dataset
+"""
+
