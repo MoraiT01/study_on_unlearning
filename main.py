392f9111b54ce84212a25dbfcd45dae43ebf088b
MoraTHI01 <moritz.kastler@thi.de>
2025-04-29 09:03:12 +0200

    prep for SLURM

---
+++ main.py
@@ -0,0 +1,236 @@
+"""This Python file exist for Slurm to run"""
+
+import os
+import sys
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.join('..', 'src'))
+
+import torch
+from PIL import Image
+import numpy as np
+
+from src.mlp_dataclass import MNIST_CostumDataset, TwoLayerPerceptron, ConvNet
+from src.training import main, train_n_models
+from src.helper import load_models_dict
+
+USED_DATASET = "mnist"
+# OUT OF: ["mnist", "cmnist", "fashion_mnist"]
+
+#########################
+ALLREADY_TRAINED = False # <--- The Switch
+#########################
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+all_data = MNIST_CostumDataset(
+    sample_mode="all",
+    train= True,
+    test = True,
+    dataset_name = USED_DATASET,
+    download=True,
+)
+
+unlearned_data = MNIST_CostumDataset(
+    sample_mode="only_erased",
+    train= True,
+    test = False,
+    dataset_name = USED_DATASET,
+    download=True,
+)
+
+def prepare_sample(path:str):
+    """preps sample to pass to model, transforms to tensor, reshape to 784, shaping it as batch"""
+    sample = Image.open(path).convert("L")
+    sample = torch.Tensor(np.array(sample)).reshape(784)
+    
+    return sample.unsqueeze(0)
+
+def show_sample(sample:torch.Tensor):
+    """Converts the sample to a numpy array and reshapes it to a 28x28 image"""
+    sample = sample.reshape(28, 28)
+    sample = sample.numpy()
+    sample = Image.fromarray(sample)
+    return sample
+
+from training import save_model
+#### Exact
+if not ALLREADY_TRAINED:
+    untrained_model = TwoLayerPerceptron() if USED_DATASET in ["mnist", "fashion_mnist"] else ConvNet()
+    # save it first
+    save_model(untrained_model, name="showcase_untrained", path=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}untrained", logs=False)
+
+    untrained_ms = {v: TwoLayerPerceptron() for v in range(30)} if USED_DATASET in ["mnist", "fashion_mnist"] else {v: ConvNet() for v in range(30)}
+
+    for i, model in untrained_ms.items():
+        save_model(model=model, name=i, path=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}untrained{os.sep}ensemble")
+else:
+    if USED_DATASET in ["mnist", "fashion_mnist"]:
+        untrained_showcase_all= TwoLayerPerceptron()
+    if USED_DATASET in ["cmnist"]:
+        untrained_showcase_all = ConvNet()
+    name = str(untrained_showcase_all)
+    untrained_showcase_all.load_state_dict(
+        torch.load(
+            f=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}untrained{os.sep}{name}_showcase_untrained", map_location=DEVICE,
+            weights_only=True
+            )
+        )
+    untrained_ms = load_models_dict(f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}untrained{os.sep}ensemble")
+
+trained_showcase_all = main(
+new_name="trained_showcase",
+model=None,
+sampling_mode="all",
+balanced=True,
+dataset_name=USED_DATASET,
+include_val=False,
+logs=True,
+)
+
+# Train a single model first
+if not ALLREADY_TRAINED:
+    trained_showcase_all = main(
+        new_name="trained_showcase",
+        model=None,
+        sampling_mode="all",
+        balanced=True,
+        dataset_name=USED_DATASET,
+        include_val=True,
+        logs=True,
+    )
+else:
+    if USED_DATASET in ["mnist", "fashion_mnist"]:
+        trained_showcase_all= TwoLayerPerceptron()
+    if USED_DATASET in ["cmnist"]:
+        trained_showcase_all = ConvNet()
+    name = str(trained_showcase_all)
+    trained_showcase_all.load_state_dict(
+        torch.load(
+            f=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}all{os.sep}{name}_b_trained_showcase", map_location=DEVICE,
+            weights_only=True
+            )
+        )
+if not ALLREADY_TRAINED:
+    trained_ms = train_n_models(
+        sampling_mode="all",
+        dataset_name=USED_DATASET,
+        logs=False,
+        test_ensemble=True,
+    )
+else:
+    trained_ms = load_models_dict(path=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}all{os.sep}test_ensemble")
+
+#### Exact
+
+# Train a single model first
+if not ALLREADY_TRAINED:
+    trained_showcase_except = main(
+        new_name="trained_showcase",
+        model=None,
+        sampling_mode="except_erased",
+        balanced=True,
+        dataset_name=USED_DATASET,
+        include_val=True,
+        logs=True,
+    )
+else:
+    if USED_DATASET in ["mnist", "fashion_mnist"]:
+        trained_showcase_except= TwoLayerPerceptron()
+    if USED_DATASET in ["cmnist"]:
+        trained_showcase_except = ConvNet()
+    name = str(trained_showcase_except)
+    trained_showcase_except.load_state_dict(
+        torch.load(
+            f=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}except_erased{os.sep}{name}_b_trained_showcase", map_location=DEVICE,
+            weights_only=True
+            )
+        )
+    
+if not ALLREADY_TRAINED:
+    exact_ms = train_n_models(
+        sampling_mode="except_erased",
+        dataset_name=USED_DATASET,
+        logs=False,
+        test_ensemble=True,
+    )
+else:
+    exact_ms = load_models_dict(path=f"data{os.sep}models{os.sep}{USED_DATASET}{os.sep}except_erased{os.sep}test_ensemble")
+
+from src.metrics import model_l2_norm_difference, calc_mutlimodel_metric_average
+
+# ...trained model
+a11 = calc_mutlimodel_metric_average(untrained_ms, trained_ms, metric="l2_norm")
+
+# ...exact model
+a12 = calc_mutlimodel_metric_average(untrained_ms, exact_ms, metric="l2_norm")
+
+# ...untrained model
+a21 = calc_mutlimodel_metric_average(trained_ms, exact_ms, metric="l2_norm")
+a21
+
+calc_mutlimodel_metric_average(modeltype1=trained_ms, modeltype2=trained_ms, metric="l2_norm")
+
+from src.metrics import model_layer_wise_difference
+lw_dist = {}
+for t, mu in zip(trained_ms.values(), exact_ms.values()):
+    new = model_layer_wise_difference(t, mu)
+    if len(lw_dist) == 0:
+        for name in new.keys():
+            lw_dist[name] = []
+    for name in new.keys():
+        lw_dist[name].append(new[name])
+for key in lw_dist.keys():
+    lw_dist[key] = sum(lw_dist[key])/len(lw_dist[key])
+lw_dist
+
+
+from src.metrics import calc_mutlimodel_metric_average
+from torch.utils.data import DataLoader
+dataloader = DataLoader(
+    dataset=unlearned_data,
+    batch_size=8,
+    shuffle=False,
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=untrained_ms,
+    modeltype2=trained_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=untrained_ms,
+    modeltype2=exact_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=trained_ms,
+    modeltype2=untrained_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=trained_ms,
+    modeltype2=exact_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=exact_ms,
+    modeltype2=untrained_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
+
+calc_mutlimodel_metric_average(
+    modeltype1=exact_ms,
+    modeltype2=trained_ms,
+    testing_loader=dataloader,
+    metric="kl_div",
+)
